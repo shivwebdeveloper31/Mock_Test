f726db9790d4905353741784134c05fe314afad8
Shradhanand Patil (Network18 Associates) <shradhanand.patil@nw18.com>
2025-02-06 19:22:35 +0530

    initial commit

--- src/Components/BooksList.jsx
+++ src/Components/BooksList.jsx
@@ -1,3 +1,5 @@
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 function BooksList({ booksList, kyeName }) {
@@ -11,14 +13,22 @@ function BooksList({ booksList, kyeName }) {
         const booksName = h.split(" ")[0];
         const filterBook = books.filter((h) => h.split(" ")[0] === booksName);
         return (
-          <div className='' key={index}>
-            <h1 className='font-semibold text-[#1b2124] py-2 px-2 hover:underline'>
-              {h}
-            </h1>
+          <div key={index}>
+            <div className=' group border flex'>
+              <h1 className='font-semibold border text-[#1b2124] py-2 px-2 hover:underline'>
+                {h}
+              </h1>
+              <span>
+                <FontAwesomeIcon
+                  icon={faArrowRight}
+                  className='group-hover:rotate-180 border py-3'
+                />
+              </span>
+            </div>
             {filterBook.map((bookName, index) => {
               return (
                 <div key={index}>
-                  <p className='px-2 py-1 text-sm text-[#757575] hover:underline'>
+                  <p className='px-2 py-1 text-sm text-[#757575] hover:underline hover:text-[#1b2124]'>
                     {bookName}
                   </p>
                 </div>
